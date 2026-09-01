8377a8e1282bbde6dfd40601bf9c51c9829221cd
specimsem <musica.droplost@gmail.com>
2026-09-01 13:01:04 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE="plantassistant-v1";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json","./assets/botanical-scene.jpg"]))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
